73f8eae1d0cf0a647010adfcd316a64fa57eb37b
Dayo Oluseye <dayo.oluseye@gmail.com>
2023-01-06 14:46:45 +0100

    type structs

--- main.go
+++ main.go
@@ -7,9 +7,32 @@ import (
 
 var s = "package"
 
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
+
+}
+
 func main() {
 	var s = "main"
 
+	var myVar myStruct
+	myVar.FirstName = "dayo"
+
+	myVar2 := myStruct{
+		FirstName: "mary",
+	}
+
+
+	log.Println("myVar is set to", myVar.FirstName)
+	log.Println("myVar2 is set to", myVar2.FirstName)
+
+	log.Println("var from a struct", myVar.printFirstName())
+	log.Println("var from a struct", myVar2.printFirstName())
+	
 	type User struct {
 		FirstName   string
 		LastName    string
