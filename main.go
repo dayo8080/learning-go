014c20f2889c8bd773bb03a45b43202519e8c081
Dayo Oluseye <dayo.oluseye@gmail.com>
2023-01-06 16:36:33 +0100

    maps and slice

--- main.go
+++ main.go
@@ -2,66 +2,55 @@ package main
 
 import (
 	"log"
-	"time"
+	"sort"
 )
 
-var s = "package"
-
-type myStruct struct {
+type User struct {
 	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-
+	LastName  string
 }
 
 func main() {
-	var s = "main"
 
-	var myVar myStruct
-	myVar.FirstName = "dayo"
+	myFirstMap := make(map[string]User)
 
-	myVar2 := myStruct{
-		FirstName: "mary",
+	me := User{
+		FirstName: "Dayo",
+		LastName:  "Oluseye",
 	}
 
+	myFirstMap["me"] = me
+	myMap := make(map[string]string)
 
-	log.Println("myVar is set to", myVar.FirstName)
-	log.Println("myVar2 is set to", myVar2.FirstName)
+	myMap["dog"] = "samson"
 
-	log.Println("var from a struct", myVar.printFirstName())
-	log.Println("var from a struct", myVar2.printFirstName())
-	
-	type User struct {
-		FirstName   string
-		LastName    string
-		PhoneNumber string
-		Age         int
-		BirthDate   time.Time
-	}
+	myMap["other-dog"] = "cassie"
 
-	user := User{
-		FirstName:   "Dayo",
-		LastName:    "Oluseye",
-		Age:         40,
-		PhoneNumber: "07085405644",
-	}
+	log.Println(myMap["dog"])
+	log.Println(myMap["other-dog"])
+	log.Println(myFirstMap["me"])
 
-	log.Println(user.FirstName, user.LastName, user.BirthDate)
+	var mySlice []string
+	mySlice = append(mySlice, "This")
+	mySlice = append(mySlice, "is")
+	mySlice = append(mySlice, "a")
+	mySlice = append(mySlice, "slice")
 
-	log.Println("s is", s)
-	log.Println("s2 is", s)
+	var noSlice []int
+	numSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	noSlice = append(noSlice, 40)
+	noSlice = append(noSlice, 5)
+	noSlice = append(noSlice, 10)
 
-	i, j := saySomething("SAY")
-	log.Println(i, j)
+	log.Println(noSlice)
 
-	log.Println(saySomething("SUP"))
+	sort.Ints(noSlice)
 
-}
+	log.Println(mySlice)
+	log.Println(noSlice)
+	log.Println(numSlice)
+	log.Println(numSlice[:4])
+	log.Println(numSlice[2:6])
+	log.Println(numSlice[3:])
 
-func saySomething(s3 string) (string, string) {
-	log.Println("s from the saySomething func is", s)
-	log.Println("s from the saySomething func is", s3)
-	return s3, "Hello"
 }
