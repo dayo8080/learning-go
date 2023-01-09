6787ec179909f6345106c314fbb338ba72101f32
Dayo Oluseye <dayo.oluseye@gmail.com>
2023-01-09 12:05:43 +0100

    packages

--- main.go
+++ main.go
@@ -1,65 +1,17 @@
 package main
 
-import "log"
+import (
+	"log"
 
-// defining an interface with a name of Animal, all function inside the interface must be implemented
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-// create a Dog struct
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-// create a gorilla struct
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+	"github.com/dayo8080/learning-golang/helpers"
+)
 
 func main() {
-	// create an object of Dog struct
-	dog := Dog{
-		Name:  "Samson",
-		Breed: "German Shepherd",
-	}
-	// create an object of Gorilla struct
-	gorilla := Gorilla{
-		Name:          "Gree",
-		Color:         "Black",
-		NumberOfTeeth: 34,
-	}
-	// call the printinfo func with object dog as the parameter
-	PrintInfo(dog)
-	// call the printinfo func with object gorilla as the parameter
-	PrintInfo(gorilla)
-}
+	var myVar helpers.SomeType
 
-// create a function Says with a receivable g of Gorilla struct, which returns a string
-func (g Gorilla) Says() string {
-	return "Killll"
-}
+	myVar.FirstName = "Dayo"
+	myVar.LastName = "Oluseye"
 
-// create a function NumberOfLegs with a receivable g of Gorilla struct, which returns a int
-func (g Gorilla) NumberOfLegs() int {
-	return 6
-}
+	log.Println(myVar.FirstName, myVar.LastName)
 
-// create a function Says with a receivable d of Dog struct, which returns a string
-func (d Dog) Says() string {
-	return "woof"
-}
-
-// create a function NumberOfLegs with a receivable d of Dog struct, which returns a int
-func (d Dog) NumberOfLegs() int {
-	return 4
-}
-
-// create a function PrintInfo with an argument a as the interface
-func PrintInfo(a Animal) {
-	log.Println("This animal says", a.Says(), "and have a number of ", a.NumberOfLegs(), "legs")
 }
